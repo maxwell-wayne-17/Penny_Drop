f033effc17c6fa9a331af40e3c621a98f589f62b
Maxwell Wayne <maxwell.wayne.17@cnu.edu>
2022-03-09 12:40:41 -0500

    Finished setting up ViewModels, about to start using them (Page 63)

---
+++ app/src/main/java/dev/mwayne/pennydrop/viewmodels/PickPlayersViewModel.kt
@@ -0,0 +1,18 @@
+package dev.mwayne.pennydrop.viewmodels
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import dev.mwayne.pennydrop.types.NewPlayer
+
+class PickPlayersViewModel : ViewModel() {
+
+    val players = MutableLiveData<List<NewPlayer>>().apply {
+        this.value = (1..6).map{
+            NewPlayer(
+                canBeRemoved = it > 2,
+                canBeToggled = it > 1
+            )
+        }
+    }
+
+}
