31a9e6d8ff8925317d4d7278c2be7a4bc1536243
maxwell-wayne-17 <maxwell.wayne.17@cnu.edu>
2022-03-25 09:46:47 -0400

    Left off on "Add Some Color to the Player Type Switch" on page 73

--- app/src/main/java/dev/mwayne/pennydrop/types/NewPlayer.kt
+++ app/src/main/java/dev/mwayne/pennydrop/types/NewPlayer.kt
@@ -1,11 +1,19 @@
 package dev.mwayne.pennydrop.types
 
 import androidx.databinding.ObservableBoolean
+import dev.mwayne.pennydrop.game.AI
 
 data class NewPlayer(
     var playerName: String = "",
     val isHuman: ObservableBoolean = ObservableBoolean(true),
     val canBeRemoved: Boolean = true,
     val canBeToggled: Boolean = true,
-    var isIncluded: ObservableBoolean = ObservableBoolean(!canBeRemoved)
-)
+    var isIncluded: ObservableBoolean = ObservableBoolean(!canBeRemoved),
+    var selectedAIPosition: Int = -1
+) {
+    fun selectedAI() = if (!isHuman.get()) {
+        AI.basicAI.getOrNull(selectedAIPosition)
+    } else{
+        null
+    }
+}
