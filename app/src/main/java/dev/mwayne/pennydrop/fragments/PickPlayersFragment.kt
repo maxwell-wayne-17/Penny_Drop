1411a3bf52312ac35961e1dd21d7a0261ab038aa
maxwell-wayne-17 <maxwell.wayne.17@cnu.edu>
2022-04-05 18:58:06 -0400

    Removed automated code from PickPlayersFragment.kt

--- app/src/main/java/dev/mwayne/pennydrop/fragments/PickPlayersFragment.kt
+++ app/src/main/java/dev/mwayne/pennydrop/fragments/PickPlayersFragment.kt
@@ -12,11 +12,6 @@ import dev.mwayne.pennydrop.databinding.FragmentPickPlayersBinding
 import dev.mwayne.pennydrop.viewmodels.GameViewModel
 import dev.mwayne.pennydrop.viewmodels.PickPlayersViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Use the [PickPlayersFragment.newInstance] factory method to
@@ -54,23 +49,4 @@ class PickPlayersFragment : Fragment() {
         return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PickPlayersFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PickPlayersFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
