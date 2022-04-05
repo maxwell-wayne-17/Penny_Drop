b0e40f6864b1718189b352b1bcc033f8b69eea80
maxwell-wayne-17 <maxwell.wayne.17@cnu.edu>
2022-04-05 12:07:12 -0400

    Left off on page 80 (Check to make sure code is correct)

--- app/src/main/java/dev/mwayne/pennydrop/viewmodels/GameViewModel.kt
+++ app/src/main/java/dev/mwayne/pennydrop/viewmodels/GameViewModel.kt
@@ -1,6 +1,14 @@
 package dev.mwayne.pennydrop.viewmodels
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import dev.mwayne.pennydrop.types.Player
 
 class GameViewModel: ViewModel() {
+    private var players: List<Player> = emptyList()
+
+    fun startGame(playersForNewGame: List<Player>) {
+        this.players = playersForNewGame
+        // More logic will be added here later
+    }
 }
