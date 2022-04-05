80d84763418c51487971f6b74a9c7d7bc1fbc9a2
maxwell-wayne-17 <maxwell.wayne.17@cnu.edu>
2022-04-05 18:54:50 -0400

    Now sends info about current players into the GameViewModel in the PickPlayersFragment.kt (page 81)

--- app/src/main/java/dev/mwayne/pennydrop/fragments/PickPlayersFragment.kt
+++ app/src/main/java/dev/mwayne/pennydrop/fragments/PickPlayersFragment.kt
@@ -6,8 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import dev.mwayne.pennydrop.R
 import dev.mwayne.pennydrop.databinding.FragmentPickPlayersBinding
+import dev.mwayne.pennydrop.viewmodels.GameViewModel
 import dev.mwayne.pennydrop.viewmodels.PickPlayersViewModel
 
 // TODO: Rename parameter arguments, choose names that match
@@ -21,21 +23,11 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class PickPlayersFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
 
     private val pickPlayersViewModel
             by activityViewModels<PickPlayersViewModel>()
+    private val gameViewModel
+        by activityViewModels<GameViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -45,6 +37,18 @@ class PickPlayersFragment : Fragment() {
             .inflate(inflater, container, false)
             .apply {
                 this.vm = pickPlayersViewModel
+
+                this.buttonPlayGame.setOnClickListener {
+                    gameViewModel.startGame(
+                        pickPlayersViewModel.players.value
+                            ?.filter { newPlayer ->
+                                newPlayer.isIncluded.get()
+                            }?.map { newPlayer ->
+                                newPlayer.toPlayer()
+                            } ?: emptyList()
+                    )
+                    findNavController().navigate(R.id.gameFragment)
+                }
             }
 
         return binding.root
