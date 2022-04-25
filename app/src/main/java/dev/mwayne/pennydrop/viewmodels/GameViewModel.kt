c5d83528ddda04b0b8984a3a986a879eec6c4d78
Maxwell Wayne <maxwell.wayne.17@cnu.edu>
2022-04-24 23:10:46 -0400

    Finished chapter 4 (page 113)

--- app/src/main/java/dev/mwayne/pennydrop/viewmodels/GameViewModel.kt
+++ app/src/main/java/dev/mwayne/pennydrop/viewmodels/GameViewModel.kt
@@ -2,11 +2,15 @@ package dev.mwayne.pennydrop.viewmodels
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dev.mwayne.pennydrop.game.GameHandler
+import dev.mwayne.pennydrop.game.TurnEnd
 import dev.mwayne.pennydrop.game.TurnResult
 import dev.mwayne.pennydrop.types.Player
 import dev.mwayne.pennydrop.types.Slot
 import dev.mwayne.pennydrop.types.clear
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class GameViewModel: ViewModel() {
     private var players: List<Player> = emptyList()
@@ -22,6 +26,7 @@ class GameViewModel: ViewModel() {
 
     val currentTurnText = MutableLiveData("")
     val currentStandingsText = MutableLiveData("")
+    private var clearText = true;
 
     fun startGame(playersForNewGame: List<Player>) {
         this.players = playersForNewGame
@@ -81,6 +86,23 @@ class GameViewModel: ViewModel() {
         if (!result.isGameOver && result.currentPlayer?.isHuman == false){
             canRoll.value = false
             canPass.value = false
+            playAITurn()
+        }
+    }
+
+    private fun playAITurn() {
+        viewModelScope.launch {
+            delay(1000)
+            slots.value?.let {  currentSlots ->
+                val currentPlayer = players.firstOrNull { it.isRolling }
+
+                if(currentPlayer != null && !currentPlayer.isHuman) {
+                    GameHandler.playAITurn(players, currentPlayer,
+                        currentSlots, canPass.value == true)?.let { result ->
+                        updateFromGameHandler(result)
+                    }
+                }
+            }
         }
     }
 
@@ -96,6 +118,32 @@ class GameViewModel: ViewModel() {
             slot.lastRolled = true
         }
         slots.notifyChange()
+    }
+
+    private fun generateTurnText(result: TurnResult): String {
+        if (clearText) { currentTurnText.value = "" }
+        clearText = result.turnEnd != null
+
+        val currentText = currentTurnText.value ?: ""
+        val currentPlayerName = result.currentPlayer?.playerName ?: "???"
+        val previousPlayerName = result.previousPlayer?.playerName ?: "???"
+
+        return when {
+            result.isGameOver ->
+                """
+                |Game Over!
+                |$currentPlayerName is the winner!
+                |
+                |${generateCurrentStandings(this.players, "Final Scores:\n")}
+                """.trimMargin()
+            // There was a bug when the turnEnd conditions would display the next players name
+            // Thus, previous player is used to correct that
+            result.turnEnd == TurnEnd.Bust ->
+                "$currentText\n$previousPlayerName rolled a ${result.lastRoll} and busted!"
+            result.turnEnd == TurnEnd.Pass -> "$currentText\n$previousPlayerName passed."
+            result.lastRoll != null -> "$currentText\n$currentPlayerName rolled a ${result.lastRoll}."
+            else -> ""
+        }
     }
 
     // Mutable live data will only notify a change if the value of it is changed, not if something
